9c844847dca5a8d25b69187cf44fda4661213e4f
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-06-27 15:11:46 +0200

    ajout index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include './includes/header.php';
+include './includes/main.php';
+include './includes/footer.php';
+
+// require './includes/header.php';
+// require './includes/main.php';
+// require './includes/footer.php';
+
+// include renvoie un avertissement simple 
+// require renvoie une erreur fatale
+
+// requiere once 
